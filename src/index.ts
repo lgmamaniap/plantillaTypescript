791ed371cab1b3d0123bf59a9dd1b0842c292461
gustavo mamani <lgmamaniap@gmail.com>
2022-12-28 12:08:42 -0400

    fix: correcciones menores

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express from 'express'
 const app = express()
-app.use(express.json()) 
+app.use(express.json())
 
 const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log('someone pinged here!!')
-    res.send('pong')
+  console.log('someone pinged here!!')
+  res.send('pong')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-	
+  console.log(`Server running on port ${PORT}`)
 })
